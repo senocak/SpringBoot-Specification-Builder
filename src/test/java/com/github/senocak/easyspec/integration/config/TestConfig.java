305f86069b163f7ba5a1f969f98bd90b48333dc6
TURKCELL\TCASENOCAK <anil.senocak@turkcell.com.tr>
2025-11-25 19:56:27 +0300

    Update package references in TestConfig for integration

--- src/test/java/com/github/senocak/easyspec/integration/config/TestConfig.java
+++ src/test/java/com/github/senocak/easyspec/integration/config/TestConfig.java
@@ -6,9 +6,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Configuration
-@SpringBootApplication(scanBasePackages = "com.example.easyspec.integration")
-@EnableJpaRepositories(basePackages = "com.example.easyspec.integration.repository")
-@EntityScan(basePackages = "com.example.easyspec.integration.entity")
+@SpringBootApplication(scanBasePackages = "com.github.senocak.easyspec.integration")
+@EnableJpaRepositories(basePackages = "com.github.senocak.easyspec.integration.repository")
+@EntityScan(basePackages = "com.github.senocak.easyspec.integration.entity")
 public class TestConfig {
 }
 
